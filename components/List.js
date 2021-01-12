a7138b353efd4d935045748e726a11296dfd2030
Giang Pham <conmeobeo96@gmail.com>
2021-01-13 00:36:13 +0200

    Add http-b

--- components/List.js
+++ components/List.js
@@ -1,10 +1,31 @@
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
-import React from 'react';
-import {useLoadMedia} from '../hooks/ApiHooks';
+import React, {useEffect, useState} from 'react';
+
+const url = 'http://media.mw.metropolia.fi/wbma/media';
 
 const List = () => {
-  const mediaArray = useLoadMedia();
+  const [mediaArray, setMediaArray] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        return await response.json();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    const enrichImageData = async (item) => {
+      const response = await fetch(`${url}/${item.file_id}`);
+      const json = await response.json();
+      item.thumbnails = {w160: json.thumbnails.w160};
+      return item;
+    };
+    fetchData()
+        .then((items) => Promise.all(items.map((i) => enrichImageData(i))))
+        .then((items) => setMediaArray(items));
+  }, []);
+
   return (
     <FlatList
       data={mediaArray}
